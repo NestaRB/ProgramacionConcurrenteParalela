08c7e3000910e2521efe8946ae6bccb9dfeba381
nesta <al425992@uji.es>
2025-10-20 12:02:07 +0200

    Lab03

---
+++ EI_1024/src/Lab03/EjemploCuentaIncrementos.java
@@ -0,0 +1,94 @@
+package Lab03;
+
+// ============================================================================
+class CuentaIncrementos {
+    // ============================================================================
+    int numIncrementos = 0;
+    
+    // --------------------------------------------------------------------------
+    void incrementaNumIncrementos() {
+        numIncrementos++;
+    }
+    
+    // --------------------------------------------------------------------------
+    int dameNumIncrementos() {
+        return( numIncrementos );
+    }
+}
+
+
+// ============================================================================
+class MiHebra extends Thread {
+    // ============================================================================
+    int                numIters;
+    CuentaIncrementos  c;
+    
+    // --------------------------------------------------------------------------
+    public MiHebra( int numIters, CuentaIncrementos c ) {
+        this.numIters = numIters;
+        this.c        = c;
+    }
+    
+    // --------------------------------------------------------------------------
+    public void run() {
+        for( int i = 0; i < numIters; i++ ) {
+            c.incrementaNumIncrementos();
+        }
+    }
+}
+
+// ============================================================================
+class EjemploCuentaIncrementos {
+    // ============================================================================
+    
+    // --------------------------------------------------------------------------
+    public static void main( String args[] ) {
+        long    t1, t2;
+        double  tt;
+        int     numHebras, numIters;
+        
+        // Comprobacion y extraccion de los argumentos de entrada.
+        if( args.length != 2 ) {
+            System.err.println( "Uso: java programa <numHebras> <numIters>" );
+            System.exit( -1 );
+        }
+        try {
+            numHebras = Integer.parseInt( args[ 0 ] );
+            numIters  = Integer.parseInt( args[ 1 ] );
+            if( ( numHebras <= 0 ) || ( numIters <= 0 ) ) {
+                System.err.print( "Uso: [ java programa <numHebras> <n> ] " );
+                System.err.println( "donde ( numHebras > 0 )  y ( numIters > 0 )" );
+                System.exit( -1 );
+            }
+        } catch( NumberFormatException ex ) {
+            numHebras = -1;
+            numIters  = -1;
+            System.out.println( "ERROR: Argumentos numericos incorrectos." );
+            System.exit( -1 );
+        }
+        
+        System.out.println( "numHebras: " + numHebras );
+        System.out.println( "numIters : " + numIters );
+        
+        System.out.println( "Creando y arrancando " + numHebras + " hebras." );
+        t1 = System.nanoTime();
+        MiHebra v[] = new MiHebra[ numHebras ];
+        CuentaIncrementos c = new CuentaIncrementos();
+        for( int i = 0; i < numHebras; i++ ) {
+            v[ i ] = new MiHebra( numIters, c );
+            v[ i ].start();
+        }
+        for( int i = 0; i < numHebras; i++ ) {
+            try {
+                v[ i ].join();
+            } catch( InterruptedException ex ) {
+                ex.printStackTrace();
+            }
+        }
+        t2 = System.nanoTime();
+        tt = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+        System.out.println( "Total de incrementos: " + c.dameNumIncrementos() );
+        System.out.println( "Tiempo transcurrido en segs.: " + tt );
+    }
+}
+
---
+++ EI_1024/src/Lab03/EjemploMuestraPrimosEnVector.java
@@ -0,0 +1,148 @@
+// ===========================================================================
+public class EjemploMuestraPrimosEnVector {
+// ===========================================================================
+
+  // -------------------------------------------------------------------------
+  public static void main( String args[] ) {
+    int     numHebras, vectOpt;
+    boolean option = true;
+    long    t1, t2;
+    double  ts, tc, tb, td;
+
+    // Comprobacion y extraccion de los argumentos de entrada.
+    if( args.length != 2 ) {
+      System.err.println( "Uso: java programa <numHebras> <vectOpt>" );
+      System.exit( -1 );
+    }
+    try {
+      numHebras = Integer.parseInt( args[ 0 ] );
+      vectOpt   = Integer.parseInt( args[ 1 ] );
+      if( ( numHebras <= 0 ) || ( ( vectOpt != 0 ) && ( vectOpt != 1 ) ) ){
+          System.err.print( "Uso: [ java programa <numHebras> <vecOpt> ] " );
+          System.err.println( "donde ( numHebras > 0 )  y ( vectOpt es 0 o 1 )" );
+          System.exit( -1 );
+      } else {
+        option = (vectOpt == 0);
+      }
+    } catch( NumberFormatException ex ) {
+      numHebras = -1;
+      System.out.println( "ERROR: Argumentos numericos incorrectos." );
+      System.exit( -1 );
+    }
+
+    //
+    // Eleccion del vector de trabajo
+    //
+    VectorNumeros vn = new VectorNumeros (option);
+    long vectorTrabajo[] = vn.vector;
+
+    //
+    // Implementacion secuencial.
+    //
+    System.out.println( "" );
+    System.out.println( "Implementacion secuencial." );
+    t1 = System.nanoTime();
+    for( int i = 0; i < vectorTrabajo.length; i++ ) {
+      if( esPrimo( vectorTrabajo[ i ] ) ) {
+        System.out.println( "  Encontrado primo: " + vectorTrabajo[ i ] );
+      }
+    }
+    t2 = System.nanoTime();
+    ts = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Tiempo secuencial (seg.):                    " + ts );
+/*
+    //
+    // Implementacion paralela ciclica.
+    //
+    System.out.println( "" );
+    System.out.println( "Implementacion paralela ciclica." );
+    t1 = System.nanoTime();
+    // Gestion de hebras para la implementacion paralela ciclica
+    // (A) ....
+    t2 = System.nanoTime();
+    tc = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Tiempo paralela ciclica (seg.):              " + tc );
+    System.out.println( "Incremento paralela ciclica:                 " + ... ); // (B)
+    //
+    // Implementacion paralela por bloques.
+    //
+    // (C) ....
+    //
+    // Implementacion paralela dinamica.
+    //
+    // (D) ....
+*/
+  }
+
+  // -------------------------------------------------------------------------
+  static boolean esPrimo( long num ) {
+    boolean cond;
+    if( num < 2 ) {
+      cond = false;
+    } else {
+      cond = true;
+      long i = 2;
+      while( ( i < num )&&( cond ) ) { 
+        cond = ( num % i != 0 );
+        i++;
+      }
+    }
+    return( cond );
+  }
+}
+
+// Definicion de las Clases Hebras
+//
+// (E) ....
+
+// ===========================================================================
+class VectorNumeros {
+// ===========================================================================
+  long    vector[];
+  // -------------------------------------------------------------------------
+  public VectorNumeros (boolean caso) {
+    if (caso) {
+      vector = new long [] {
+      200000081L, 200000083L, 200000089L, 200000093L,
+      200000107L, 200000117L, 200000123L, 200000131L,
+      200000161L, 200000183L, 200000201L, 200000209L,
+      200000221L, 200000237L, 200000239L, 200000243L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L
+      };
+    } else {
+      vector = new long [] {
+      200000081L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000083L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000089L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000093L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000107L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000117L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000123L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000131L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000161L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000183L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000201L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000209L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 
+      200000221L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000237L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000239L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000243L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L
+      };
+    }
+  }
+}
+
