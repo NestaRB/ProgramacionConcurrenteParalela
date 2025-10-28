aea36c10c7cedab8068da6498464bf5b6fd69fe3
nesta <al425992@uji.es>
2025-10-28 16:40:05 +0100

    Lab4 inicial

---
+++ EI_1024/src/Lab04/EjemploNumeroPI.java
@@ -0,0 +1,187 @@
+package Lab04;/*
+// ===========================================================================
+class Acumula {
+// ===========================================================================
+  double  suma;
+
+  // -------------------------------------------------------------------------
+  Acumula() {
+    // ...
+  }
+
+  // -------------------------------------------------------------------------
+  void acumulaDato( double dato ) {
+    // ...
+  }
+
+  // -------------------------------------------------------------------------
+  double dameDato() {
+    // ...
+  }
+}
+
+// ===========================================================================
+class MiHebraMultAcumulaciones extends Thread {
+// ===========================================================================
+  int      miId, numHebras;
+  long     numRectangulos;
+  Acumula  a;
+
+  // -------------------------------------------------------------------------
+  MiHebraMultAcumulaciones( int miId, int numHebras, long numRectangulos,
+                              Acumula a ) {
+    // ...
+  }
+
+  // -------------------------------------------------------------------------
+  public void run() {
+    // ...
+  }
+}
+
+// ===========================================================================
+class MiHebraUnaAcumulacion extends Thread {
+// ===========================================================================
+// ...
+}
+
+// ===========================================================================
+class MiHebraMultAcumulacionAtomica extends Thread {
+// ===========================================================================
+// ...
+}
+
+// ===========================================================================
+class MiHebraUnaAcumulacionAtomica extends Thread {
+// ===========================================================================
+// ...
+}
+
+*/
+
+// ===========================================================================
+class EjemploNumeroPI {
+// ===========================================================================
+
+  // -------------------------------------------------------------------------
+  public static void main( String args[] ) {
+    long                        numRectangulos;
+    double                      baseRectangulo, x, suma, pi;
+    int                         numHebras;
+    long                        t1, t2;
+    double                      tSec, tPar;
+    // Acumula                     a;
+    // MiHebraMultAcumulaciones  vt[];
+
+    // Comprobacion de los argumentos de entrada.
+    if( args.length != 2 ) {
+      System.out.println( "ERROR: numero de argumentos incorrecto.");
+      System.out.println( "Uso: java programa <numHebras> <numRectangulos>" );
+      System.exit( -1 );
+    }
+    try {
+      numHebras      = Integer.parseInt( args[ 0 ] );
+      numRectangulos = Long.parseLong( args[ 1 ] );
+      if( ( numHebras <= 0 ) || ( numRectangulos <= 0 ) ) {
+        System.err.print( "Uso: [ java programa <numHebras> <n> ] " );
+        System.err.println( "donde ( numHebras > 0 ) y ( numRectangulos > 0 )" );
+        System.exit( -1 );
+      }
+    } catch( NumberFormatException ex ) {
+      numHebras      = -1;
+      numRectangulos = -1;
+      System.out.println( "ERROR: Numeros de entrada incorrectos." );
+      System.exit( -1 );
+    }
+
+    System.out.println();
+    System.out.println( "Calculo del numero PI mediante integracion." );
+
+    //
+    // Calculo del numero PI de forma secuencial.
+    //
+    System.out.println();
+    System.out.println( "Inicio del calculo secuencial." );
+    t1 = System.nanoTime();
+    baseRectangulo = 1.0 / ( ( double ) numRectangulos );
+    suma           = 0.0;
+    for( long i = 0; i < numRectangulos; i++ ) {
+      x = baseRectangulo * ( ( ( double ) i ) + 0.5 );
+      suma += f( x );
+    }
+    pi = baseRectangulo * suma;
+    t2 = System.nanoTime();
+    tSec = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Version secuencial. Numero PI: " + pi );
+    System.out.println( "Tiempo secuencial (s.):        " + tSec );
+/*
+    //
+    // Calculo del numero PI de forma paralela: 
+    // Multiples acumulaciones por hebra.
+    //
+    System.out.println();
+    System.out.print( "Inicio del calculo paralelo: " );
+    System.out.println( "Multiples acumulaciones por hebra." );
+    t1 = System.nanoTime();
+    // ...
+    t2 = System.nanoTime();
+    tPar = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Calculo del numero PI:   " + pi );
+    System.out.println( "Tiempo ejecucion (s.):   " + tPar );
+    System.out.println( "Incremento velocidad :   " + ... );
+
+    //
+    // Calculo del numero PI de forma paralela: 
+    // Una acumulacion por hebra.
+    //
+    System.out.println();
+    System.out.print( "Inicio del calculo paralelo: " );
+    System.out.println( "Una acumulacion por hebra." );
+    t1 = System.nanoTime();
+    // ...
+    t2 = System.nanoTime();
+    tPar = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Calculo del numero PI:   " + pi );
+    System.out.println( "Tiempo ejecucion (s.):   " + tPar );
+    System.out.println( "Incremento velocidad :   " + ... );
+
+    //
+    // Calculo del numero PI de forma paralela: 
+    // Multiples acumulaciones por hebra (Atomica)
+    //
+    System.out.println();
+    System.out.print( "Inicio del calculo paralelo: " );
+    System.out.println( "Multiples acumulaciones por hebra (At)." );
+    t1 = System.nanoTime();
+    // ...
+    t2 = System.nanoTime();
+    tPar = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Calculo del numero PI:   " + pi );
+    System.out.println( "Tiempo ejecucion (s.):   " + tPar );
+    System.out.println( "Incremento velocidad :   " + ... );
+
+    //
+    // Calculo del numero PI de forma paralela: 
+    // Una acumulacion por hebra (Atomica).
+    //
+    System.out.println();
+    System.out.print( "Inicio del calculo paralelo: " );
+    System.out.println( "Una acumulacion por hebra (At)." );
+    t1 = System.nanoTime();
+    // ...
+    t2 = System.nanoTime();
+    tPar = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Calculo del numero PI:   " + pi );
+    System.out.println( "Tiempo ejecucion (s.):   " + tPar );
+    System.out.println( "Incremento velocidad :   " + ... );
+*/
+    System.out.println();
+    System.out.println( "Fin de programa." );
+  }
+
+  // -------------------------------------------------------------------------
+  static double f( double x ) {
+    return ( 4.0/( 1.0 + x*x ) );
+  }
+}
+
