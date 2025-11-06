7eef73eb3aefca714dff5c533d89b68af1512802
butifarralLoca43 <al438089@uji.es>
2025-11-06 09:58:30 +0100

    hebra virtual ej 5

---
+++ EI_1024/src/EJ5_del_lab5/MiHebraVirtual.java
@@ -0,0 +1,28 @@
+package EJ5_del_lab5;
+
+public class MiHebraVirtual implements Runnable{
+
+    private NuevoDisparoUnaHebra d;
+    private GUITiroAlBlancoUnaHebra gui;
+
+    public MiHebraVirtual(NuevoDisparoUnaHebra d,
+                          GUITiroAlBlancoUnaHebra gui) {
+        this.d = d;
+        this.gui = gui;
+    }
+    @Override
+    public void run() {
+        ProyectilUnaHebra p = new ProyectilUnaHebra(
+                d.velocidadInicial, d.anguloInicial, gui.cnvCampoTiro);
+
+        for (int i = 0; i < gui.numIters; i++) {
+            p.imprimeEstadoProyectilEnConsola();
+            p.mueveUnIncremental();
+            p.actualizaDibujoDeProyectil();
+
+            if (gui.determinaEstadoProyectil(p))
+                break;
+        }
+    }
+
+}
