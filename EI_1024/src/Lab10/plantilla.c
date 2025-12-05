472e6d25d03bc5f2230e142d8658b1757422b9ed
nesta <al425992@uji.es>
2025-12-06 00:22:34 +0100

    lab 10 inicial

---
+++ EI_1024/src/Lab10/plantilla.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+// ============================================================================
+int main( int argc, char * argv[] ) {
+  int  numProcs, miId;
+
+  // Inicializa MPI.
+  MPI_Init( &argc, &argv );
+  MPI_Comm_size( MPI_COMM_WORLD, &numProcs );
+  MPI_Comm_rank( MPI_COMM_WORLD, &miId );
+  
+  // --------------------------------------------------------------------------
+  int prc, suma, aux, dato;
+  MPI Status st ;
+  // Cada proceso dispone de un dato cualquiera.
+  dato = numProcs − miId + 1;
+  printf( ”Proc %d Dato vale: %d \n”, miId, dato );
+ 
+  // ... Incluir codigo asociado a los ejercicios 1 y 2
+  
+  // --------------------------------------------------------------------------
+
+  // Finalizacion de MPI.
+  MPI_Finalize();
+
+  // Fin de programa.
+  printf( "Final de programa (%d) \n", miId );
+  return 0;
+}
+
